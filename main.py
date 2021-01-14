0839fdd54760c7278460c7b70ba5ec3c1e7bc7f3
KatyMartin23 <832006@mystma.org>
2021-01-14 15:50:34 +0000

    calculate 1

--- main.py
+++ main.py
@@ -1,1 +1,9 @@
-#WRITE YOUR CODE IN THIS FILE
+def calculate(c, x, y):
+    if c == "add":
+        return x + y
+    elif c == "subtract":
+        return x - y
+    elif c == "multiply":
+        return x * y
+    else: return x / y
+print(calculate("add", 5, 5))
